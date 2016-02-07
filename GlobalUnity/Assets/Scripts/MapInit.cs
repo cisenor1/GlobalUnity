98e590de9fdee42cc4773977a3dde5b8f8984122
Craig Isenor <craig@educask.com>
2016-02-07 13:28:58 -0800

    Switched to mapping it in 2d.

---
+++ GlobalUnity/Assets/Scripts/MapInit.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MapInit : MonoBehaviour {
+
+    public float MinHeight;
+    public float MaxHeight;
+    private Mesh mesh;
+
+	// Use this for initialization
+	void Start () {
+        //mesh = CreateMesh(20, 20, 40, 40, false);
+        //gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        var m = gameObject.GetComponent<MeshFilter>().mesh;
+        m.Clear();
+        
+        FillMesh(m, 250, 250, false);
+        
+        //gameObject.AddComponent<MeshRenderer>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+    }
+    
+    void FillMesh(Mesh m, int widthSegments, int lengthSegments, bool twoSided, string name="PLANEMESH")
+    {
+        // Top-left
+        //Vector2 anchorOffset = new Vector2(-width / 2.0f, length / 2.0f);
+        // Center
+        
+        Vector2 anchorOffset = Vector2.zero;
+        int hCount2 = widthSegments + 1;
+        int vCount2 = lengthSegments + 1;
+        int numTriangles = widthSegments * lengthSegments * 6;
+        if (twoSided)
+        {
+            numTriangles *= 2;
+        }
+        int numVertices = hCount2 * vCount2;
+
+        Vector3[] vertices = new Vector3[numVertices];
+        Vector2[] uvs = new Vector2[numVertices];
+        int[] triangles = new int[numTriangles];
+        Vector4[] tangents = new Vector4[numVertices];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
+        int index = 0;
+        float uvFactorX = 1.0f / widthSegments;
+        float uvFactorY = 1.0f / lengthSegments;
+        //float scaleX = width / widthSegments;
+        //float scaleY = length / lengthSegments;
+        for (float y = 0.0f; y < vCount2; y++)
+        {
+            for (float x = 0.0f; x < hCount2; x++)
+            {
+                vertices[index] = new Vector3(x - anchorOffset.x, UnityEngine.Random.Range(MinHeight, MaxHeight), y - anchorOffset.y);
+                tangents[index] = tangent;
+                uvs[index++] = new Vector2(x * uvFactorX, y * uvFactorY);
+            }
+        }
+
+        index = 0;
+        for (int y = 0; y < lengthSegments; y++)
+        {
+            for (int x = 0; x < widthSegments; x++)
+            {
+                triangles[index] = (y * hCount2) + x;
+                triangles[index + 1] = ((y + 1) * hCount2) + x;
+                triangles[index + 2] = (y * hCount2) + x + 1;
+
+                triangles[index + 3] = ((y + 1) * hCount2) + x;
+                triangles[index + 4] = ((y + 1) * hCount2) + x + 1;
+                triangles[index + 5] = (y * hCount2) + x + 1;
+                index += 6;
+            }
+            if (twoSided)
+            {
+                // Same tri vertices with order reversed, so normals point in the opposite direction
+                for (int x = 0; x < widthSegments; x++)
+                {
+                    triangles[index] = (y * hCount2) + x;
+                    triangles[index + 1] = (y * hCount2) + x + 1;
+                    triangles[index + 2] = ((y + 1) * hCount2) + x;
+
+                    triangles[index + 3] = ((y + 1) * hCount2) + x;
+                    triangles[index + 4] = (y * hCount2) + x + 1;
+                    triangles[index + 5] = ((y + 1) * hCount2) + x + 1;
+                    index += 6;
+                }
+            }
+        }
+
+        m.vertices = vertices;
+        m.uv = uvs;
+        m.triangles = triangles;
+        m.tangents = tangents;
+        m.RecalculateNormals();
+
+        //AssetDatabase.CreateAsset(m, "Assets/Editor/" + planeAssetName);
+        //AssetDatabase.SaveAssets();
+    }
+}
---
+++ GlobalUnity/Assets/Shaders/HeightMap.shader
@@ -0,0 +1,72 @@
+﻿Shader "Unlit/HeightMap"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_MinHeight("Min Height", Float) = 0
+		_MaxHeight("Max Height", Float) = 500
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float3 height;
+			float _MinHeight;
+			float _MaxHeight;
+			float scale;
+			float displacement;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				//o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				// pseudocode maybe
+				height = tex2Dlod(_MainTex, float4(o.uv,0,0));
+				scale = _MaxHeight - _MinHeight;
+				displacement = _MinHeight - scale;
+				//v.vertex.y = height.r;
+				v.vertex.y = (height.r * scale) + displacement;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
